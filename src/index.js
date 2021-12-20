f61244f9faa6e46fac6879fe82df79af596a862d
AJ Jalusi <ajalusi26@gmail.com>
2021-12-20 14:19:21 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const submit = document.querySelector('#submit-btn')
+const list = document.querySelector('#tasks')
+const input = document.querySelector('#new-task-description')
+
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    let li = document.createElement('li')
+    li.innerText = (`${input.value}  `)
+    list.append(li)
+    input.value = ""
+    deleteButton(li)
+})
+
+function deleteButton(li){
+    let button = document.createElement('button')
+    button.innerText = "x"
+    li.append(button)
+    button.addEventListener("click", () => {
+        li.style.display = 'none'
+    })
+}
